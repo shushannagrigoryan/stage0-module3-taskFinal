2e480cb6ae3594d5d312436fd0c26de17159b0ed
Shushanna <shushannagrigoryan7@gmail.com>
2022-08-15 16:47:32 +0400

    2

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -10,6 +10,7 @@ public class NumberReverter {
             temp = number%10;
             new_number = new_number * 10 + temp;
             number/= 10;
+
             //t1*=10;
         }
         System.out.println(new_number);
