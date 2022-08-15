2be3300e9800721ee78f1d5e4199d9ae792e7967
Shushanna <shushannagrigoryan7@gmail.com>
2022-08-15 17:00:39 +0400

    3

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,7 +2,6 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-        if(number > 0){System.out.println(true);}
-        else{System.out.println(false);}
+        System.out.println(number > 0);
     }
 }
